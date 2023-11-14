74be17faee9c3709dc8279ec0c2f9490698dfe88
William-Smit <150056456+William-Smit@users.noreply.github.com>
2023-11-14 21:46:35 +0000

    Adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home () {
-    return <h1>Concluido, será mesmo?</h1>    
+function Home() {
+  return <h1>Concluido, será mesmo?</h1>;
+}
+
+function teste() {
+  console.log("Teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
