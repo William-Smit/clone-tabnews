04c8546a25930076bac4c38d384e501e27b83c42
William-Smit <150056456+William-Smit@users.noreply.github.com>
2023-11-21 16:01:33 +0000

    Install  and configure test scripts

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Concluido, será mesmo?</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
